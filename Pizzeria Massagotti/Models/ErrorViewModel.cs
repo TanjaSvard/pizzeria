2a4e35e4f5aac478337e899886310b7f0ab99da1
Tanja Svärd <tanja_jazz@hotmail.com>
2017-08-23 11:10:27 +0200

    Add project files.

---
+++ Pizzeria Massagotti/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pizzeria_Massagotti.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
