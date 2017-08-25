5b4df79e063ace5fe2814bc097c9b0fe69bd5656
Tanja Svärd <tanja_jazz@hotmail.com>
2017-08-25 14:45:00 +0200

    ingredients controller

---
+++ Pizzeria Massagotti/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzeriaMassagotti.Models
+{
+    public class Cart
+    {
+        public int DishId { get; set; }
+        public int OrderId { get; set; }
+        public List<Dish> DishList { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
