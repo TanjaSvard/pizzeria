238f9ee435211432a2fa9e57c736790379c2fa4f
Tanja Svärd <tanja_jazz@hotmail.com>
2017-08-27 23:23:35 +0200

    CartitemIngredient.cs update

--- Pizzeria Massagotti/Models/CartItemIngredient.cs
+++ Pizzeria Massagotti/Models/CartItemIngredient.cs
@@ -7,6 +7,10 @@ namespace PizzeriaMassagotti.Models
 {
     public class CartItemIngredient
     {
-       
+        public int CartItemId { get; set; }
+        public CartItem Cartitem { get; set; }
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; }
+        public bool Enabled { get; set; }
     }
 }
